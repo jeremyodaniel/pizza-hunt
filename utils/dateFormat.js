503a5257861fa553c5de01b177d2f851623167ec
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-03-19 22:21:13 -0500

    Add comments to pizzas and see number of comments added

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,3 +1,4 @@
+const dateFormat = require('../utils/dateFormat');
 const addDateSuffix = date => {
   let dateStr = date.toString();
 
@@ -17,7 +18,8 @@ const addDateSuffix = date => {
   return dateStr;
 };
 
-// function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
+// function to format a timestamp, accepts the timestamp and an `options` 
+// object as optional parameters
 module.exports = (
   timestamp,
   { monthLength = 'short', dateSuffix = true } = {}
